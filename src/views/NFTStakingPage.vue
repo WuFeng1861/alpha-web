fdacd32a853d778e88e9c1450ae08a6281157d41
WuFeng1861 <71131991+WuFeng1861@users.noreply.github.com>
2025-06-30 07:34:37 +0800

    修复返回按钮黑屏问题

--- src/views/NFTStakingPage.vue
+++ src/views/NFTStakingPage.vue
@@ -145,7 +145,13 @@ onUnmounted(() => {
 
 // 返回上一页
 const goBack = () => {
-  router.back()
+  // 检查是否有历史记录可以返回
+  if (window.history.length > 1) {
+    router.back()
+  } else {
+    // 如果没有历史记录，返回到质押页面
+    router.push('/staking')
+  }
 }
 
 // 处理领取按钮
